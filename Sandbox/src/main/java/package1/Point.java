9c4d3e46687346ee73df72d559d4d7cfa055b1dc
darkfay <31184025+darkfay@users.noreply.github.com>
2020-07-23 12:46:01 +0300

    homework1 corrected

--- Sandbox/src/main/java/package1/Point.java
+++ Sandbox/src/main/java/package1/Point.java
@@ -1,19 +1,19 @@
 package package1;
 
 public class Point {
-    public double x1;
-    public double x2;
+    public double x;
+    public double y;
 
-    public Point(double x1, double x2) {
-        this.x1 = x1;
-        this.x2 = x2;
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
 
     }
 
     public double distance(Point p2) {
 
 
-        return Math.sqrt((p2.x2 - p2.x1) * (p2.x2 - p2.x1) + (this.x2 - this.x1) * (this.x2 - this.x1));
+        return Math.sqrt((p2.y - this.y) * (p2.y - this.y) + (p2.x - this.x) * (p2.x - this.x));
     }
 
 }
